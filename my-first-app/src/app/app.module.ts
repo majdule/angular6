0cf608bc1907719e764f39ad5aa044243a2bb3d3
majdule <majstorovicdule@gmail.com>
2018-07-15 10:42:03 +0200

    Creating a custom component

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule,
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
