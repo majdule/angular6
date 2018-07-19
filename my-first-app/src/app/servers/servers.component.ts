8cf4ebfefa568f438c7bf679baa4e8664d45ab18
MAJ1BG <dusan.majstorovic@bosch.com>
2018-07-19 09:16:53 +0200

    two way data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';
+  serverName = 'Testserver';
 
   constructor() {
     setTimeout(() => {
@@ -24,6 +25,10 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
