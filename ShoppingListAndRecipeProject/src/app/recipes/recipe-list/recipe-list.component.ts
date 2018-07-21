fe55caf7b3ccd92616937722af7e7e8784a9b287
majdule <majstorovicdule@gmail.com>
2018-07-21 22:28:03 +0200

    created recipe list and details

--- ShoppingListAndRecipeProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingListAndRecipeProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Pizza', 'This is a recipe for Italian Pizza.', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Croissant', 'A croissant is a buttery, flaky, viennoiserie pastry named for its crescent shape. Croissants and other viennoiserie are made of a layered yeast-leavened dough. The dough is layered with butter, rolled and folded several times in succession, then rolled into a sheet, in a technique called laminating. The process results in a layered, flaky texture, similar to a puff pastry.', 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Croissants_au_beurre_%2818953292873%29.jpg/250px-Croissants_au_beurre_%2818953292873%29.jpg')
+
+  ];
 
   constructor() { }
 
