df5e68333c8d07817a2bf520883ba1994ea7290a
majdule <majstorovicdule@gmail.com>
2018-07-22 20:25:07 +0200

    Implemented adding

--- ShoppingListAndRecipeProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ ShoppingListAndRecipeProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     // tslint:disable-next-line:max-line-length
     new Recipe('Pizza', 'This is a recipe for Italian Pizza.', 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg'),
@@ -18,6 +19,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
